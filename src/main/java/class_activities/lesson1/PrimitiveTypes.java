0986e73a2a2d0143b63a0165fb90806a4fa970d4
ElvinSadikhov <elvin.279.6s@gmail.com>
2022-06-26 11:39:56 +0400

    created file

---
+++ src/main/java/class_activities/lesson1/PrimitiveTypes.java
@@ -0,0 +1,14 @@
+package class_activities.lesson1;
+
+public class PrimitiveTypes {
+
+    boolean boolObj = true; // can be true-false / 1-0
+    byte byteObj = 32; // integers that can fit in 1 byte
+    int intObj = 12121; // integers that can fit in 2 bytes
+    long longObj = 11212100; // integers that can fit in 4 bytes
+    double doubleObj;
+    float floatObj;
+    char charObj = 'a'; // characters must be written inky using single-quotas
+    String stringObj = "Valhalla"; // Strings must be written inky using double-quotas
+
+}
