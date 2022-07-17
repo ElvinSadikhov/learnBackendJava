0c34f6e77b9604e8f8c688e482df7457ce95d41d
ElvinSadikhov <elvin.279.6s@gmail.com>
2022-07-17 23:57:32 +0400

    1st commit

---
+++ src/main/java/homeworks/hw_4_classes/Human.java
@@ -0,0 +1,55 @@
+package homeworks.hw_4_classes;
+
+public class Human {
+    String name;
+    String surname;
+    int year;
+    int iq;
+    Pet pet;
+    Human mother;
+    Human father;
+    String schedule[][];
+
+    Human(String name, String surname, int year) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+    }
+
+    Human(String name, String surname, int year, Human mother, Human father) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.mother = mother;
+        this.father = father;
+    }
+
+    Human(String name, String surname, int year, int iq, Pet pet, Human mother, Human father, String schedule[][]) {
+        this.name = name;
+        this.surname = surname;
+        this.year = year;
+        this.iq = iq;
+        this.pet = pet;
+        this.mother = mother;
+        this.father = father;
+        this.schedule = schedule;
+    }
+
+    Human() {
+
+    }
+
+    void greetPet() {
+        System.out.printf("Hello, %s\n", this.pet.nickname);
+    }
+
+    void describePet() {
+        System.out.printf("I have a %s, he is %d years old, he is %s\n",
+                this.pet.species, this.pet.age, this.pet.trickLevel > 50 ? "very sly" : "almost not sly");
+    }
+
+    public String toString(){
+        return "Human{name=" + this.name + ", surname=" + this.surname + ", year=" + this.year + ", iq=" + this.iq + ", mother=" + this.mother.name + " " + this.mother.surname + ", father=" + this.father.name + " " + this.father.surname +", " + this.pet + "}";
+    }
+
+}
---
+++ src/main/java/homeworks/hw_4_classes/Main.java
@@ -0,0 +1,11 @@
+package homeworks.hw_4_classes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Pet dog = new Pet("dog", "muxtar", 2, 35, new {"eating", "playing"});
+
+        Human elvin = new Human("ELvin", "Sadikhov", 2003, 200, );
+    }
+
+}
